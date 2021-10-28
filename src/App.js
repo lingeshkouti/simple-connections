8ed7a75378b7f138f3fbcb5e541dcf035701d26c
Lingesh <lingeshkouti@gmail.com>
2021-10-28 21:52:08 +0530

    added first changes

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import Header from './Header';
 import ProfileList from './ProfileList';
 import './App.css';
+const Head = () => <h1>Temp fix</h1>;
 
 export default class App extends Component {
     render() {
